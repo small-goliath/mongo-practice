bec279a59d8668b7e25abf91ce63b0bc5bfb7ec6
small-goliath <meang617@gmail.com>
2025-02-27 16:36:28 +0900

    init

---
+++ src/main/kotlin/org/rsup/mongopractice/MongoPracticeApplication.kt
@@ -0,0 +1,11 @@
+package org.rsup.mongopractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MongoPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MongoPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/org/rsup/mongopractice/MongoPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.rsup.mongopractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MongoPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
